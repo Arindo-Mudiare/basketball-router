e21e9d10c540af44c6eeb389bbafa42f55128579
Irapfaya <arindomudiare@gmail.com>
2021-08-10 23:32:48 +0100

    created Basic navigation and routing with 404

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
-import * as React from 'react'
+import * as React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Players from "./Players";
+import Teams from "./Teams";
 
-export default function App () {
+export default function App() {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/players">
+            <Players />
+          </Route>
+          <Route path="/teams">
+            <Teams />
+          </Route>
+          <Route path="*">
+            <h1 className="text-center">404 Not Found</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
